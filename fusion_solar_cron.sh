875e65fa05a2cb4e340628b28bd24990aeadc065
Pedro Cardoso <pedroosodracwork@gmail.com>
2021-10-08 16:27:11 +0100

    Updated Cron Job

--- fusion_solar_cron.sh
+++ fusion_solar_cron.sh
@@ -7,4 +7,4 @@ source ./env/bin/activate
 # let the PATH figure out which version to run (based on what your
 # virtualenv has configured).
 
-python screenshot.py && cp painel.png /srv/dev-disk-by-uuid-2ad6968b-221f-40b7-920f-7cf58a149d6f/Config/homeassistant
+python screenshot.py && cp painel.png /home/pi/homeassistant/config/www
